112677b07665a50683dfa6ae7a494a4ac63d5c74
Alkey <Sirj777sirj@gmail.com>
2020-09-30 14:11:32 +0300

    add Movie model, implements MovieDao, MovieService.

---
+++ src/main/java/com/dev/cinema/MainApp.java
@@ -0,0 +1,17 @@
+package com.dev.cinema;
+
+import com.dev.cinema.lib.Injector;
+import com.dev.cinema.model.Movie;
+import com.dev.cinema.service.MovieService;
+
+public class MainApp {
+    private static MovieService movieService =
+            (MovieService) Injector.getInstance("com.dev.cinema").getInstance(MovieService.class);
+
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setTitle("Star Wars");
+        System.out.println(movieService.add(movie));
+        System.out.println(movieService.getAll());
+    }
+}
