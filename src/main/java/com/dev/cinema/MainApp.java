2bb3e35792c28fb63adea363703b7b95d8d66324
Alkey <68539602+Alkey@users.noreply.github.com>
2020-10-08 11:29:43 +0300

    add User model, dao, service, AuthenticationService. (#4)
    * add User model, dao, service, AuthenticationService.
    
    * fix
    
    * fix comments
    
    * fix comment

--- src/main/java/com/dev/cinema/MainApp.java
+++ src/main/java/com/dev/cinema/MainApp.java
@@ -1,12 +1,16 @@
 package com.dev.cinema;
 
+import com.dev.cinema.exception.AuthenticationException;
 import com.dev.cinema.lib.Injector;
 import com.dev.cinema.model.CinemaHall;
 import com.dev.cinema.model.Movie;
 import com.dev.cinema.model.MovieSession;
+import com.dev.cinema.model.User;
+import com.dev.cinema.security.AuthenticationService;
 import com.dev.cinema.service.CinemaHallService;
 import com.dev.cinema.service.MovieService;
 import com.dev.cinema.service.MovieSessionService;
+import com.dev.cinema.service.UserService;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -19,6 +23,11 @@ public class MainApp {
     private static MovieSessionService sessionService =
             (MovieSessionService) Injector.getInstance("com.dev.cinema")
                     .getInstance(MovieSessionService.class);
+    private static UserService userService = (UserService) Injector.getInstance("com.dev.cinema")
+            .getInstance(UserService.class);
+    private static AuthenticationService authenticationService =
+            (AuthenticationService) Injector.getInstance("com.dev.cinema")
+            .getInstance(AuthenticationService.class);
 
     public static void main(String[] args) {
         Movie starWars = new Movie();
@@ -50,5 +59,18 @@ public class MainApp {
         System.out.println(sessionService.add(sessionSecond));
         System.out.println(sessionService.findAvailableSessions(starWars.getId(),
                 LocalDate.of(2020, 10, 6)));
+        User bob = new User();
+        bob.setEmail("Bob@gmail.com");
+        bob.setPassword("123");
+        try {
+            System.out.println(authenticationService.register(bob.getEmail(), bob.getPassword()));
+        } catch (AuthenticationException e) {
+            System.out.println(e);
+        }
+        try {
+            System.out.println(authenticationService.login(bob.getEmail(), bob.getPassword()));
+        } catch (AuthenticationException e) {
+            System.out.println(e);
+        }
     }
 }
---
+++ src/main/java/com/dev/cinema/security/AuthenticationServiceImpl.java
@@ -0,0 +1,37 @@
+package com.dev.cinema.security;
+
+import com.dev.cinema.exception.AuthenticationException;
+import com.dev.cinema.lib.Inject;
+import com.dev.cinema.lib.Service;
+import com.dev.cinema.model.User;
+import com.dev.cinema.service.UserService;
+import com.dev.cinema.util.HashUtil;
+import java.util.Optional;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> userFromDb = userService.findByEmail(email);
+        if (userFromDb.isPresent()
+                && isValid(userFromDb.get().getPassword(), password, userFromDb.get().getSalt())) {
+            return userFromDb.get();
+        }
+        throw new AuthenticationException("Incorrect email: " + email + " or password");
+    }
+
+    @Override
+    public User register(String email, String password) {
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        return userService.add(user);
+    }
+
+    private boolean isValid(String userPassword, String password, byte[] salt) {
+        return userPassword.equals(HashUtil.hashPassword(password, salt));
+    }
+}
